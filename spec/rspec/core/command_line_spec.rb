db4a8529c6d8d4aa4fa0a2c3eeb4a5836813fe64
Jon Rowe <hello@jonrowe.co.uk>
2013-10-15 15:41:43 +1100

    supress warning

--- spec/rspec/core/command_line_spec.rb
+++ spec/rspec/core/command_line_spec.rb
@@ -16,6 +16,7 @@ module RSpec::Core
     end
 
     it "configures streams before command line options" do
+      allow(RSpec).to receive(:deprecate)  # remove this and should_receive when ordered
       stdout = StringIO.new
       allow(config).to receive(:load_spec_files)
       allow(config).to receive(:reporter).and_return(double.as_null_object)
